9b8977ba083b771a2b11bcd07ab6203623e11d55
YOONJAHYUN <dbswkgus323@gmail.com>
2023-02-03 09:20:14 +0900

    try

---
+++ SWEA/6485/sol.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for tc in range(T):
+    N = int(input())
+    for line in range(N):
+        A, B = map(int, input().split())
+
+    P = int(input())
+
+    for i in range(P):
+        num = int(input())
+
+
+        print(num)
