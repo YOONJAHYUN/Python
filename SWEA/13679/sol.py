f516eabc4ed4f409af15470dbe8114debf3ca002
YOONJAHYUN <dbswkgus323@gmail.com>
2023-02-07 01:14:12 +0900

    0206

---
+++ SWEA/13679/sol.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for tc in range(T):
+    N, M = map(int, input().split())
+
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    for i in range(0, N-M+1):
+        for j in range(0, N-M+1):
+            for k in range(0, M):
