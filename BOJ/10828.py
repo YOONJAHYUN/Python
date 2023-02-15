7bdd1ec46c4aeb05b0c583a2a8eb9667274e60dc
YOONJAHYUN <dbswkgus323@gmail.com>
2023-02-16 01:27:37 +0900

    0216

--- BOJ/10828.py
+++ BOJ/10828.py
@@ -4,30 +4,30 @@ input = sys.stdin.readline
 
 N = int(input())
 
-def stack(a, b=-1):
-    lst = []
-    if a == 'push':
-        lst.append(b)
-    elif a == 'top':
-        if len(lst) == 0:
-            print(-1)
-        print(lst[-1])
-    elif a == 'size':
-        print(len(lst))
-    elif a == 'empty':
-        if stack:
-            print('0')
-        else:
-            print('1')
-    elif a == 'pop':
-        if stack:
-            print(lst[-1])
-            lst.remove(-1)
-        else:
-            print('1')
-
+stack = []
 
 for n in range(N):
-    a, b = input().split()
-    b = int(b)
-    print(stack(a, b))
+    a, *b = input().split()
+    # b = int(b)
+
+
+    if a == 'push':
+        stack.append(int(*b))
+    elif a == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+
+    elif a == 'size':
+        print(len(stack))
+    elif a == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif a == 'top':
+        if stack:
+            print(int(stack[-1]))
+        else:
+            print(-1)
