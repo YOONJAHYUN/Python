ade7818e103c70e01a6e0fffeeecb12ab4c4c2da
YOONJAHYUN <dbswkgus323@gmail.com>
2023-02-20 17:54:00 +0900

    0220Vm

--- BOJ/15650.py
+++ BOJ/15650.py
@@ -1,28 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-def f(n, m, depth):
-    global num, combi
-
+def f(n, m, depth = 0, i = 0, combi = []):
     if depth == m:
-        if len(combi) == m:
-            num.append(combi)
-
+        print(*combi)
         return
-
+    # 이전단계에서 뭘 받을건지부터..
+    # 받아주는거랑 넘겨주는게 같아야한다.
     # 순회하고, i를 다음단계
-    for i in range(1, n+1):
-
-
-        combi.append(i)
-
-        f(n, m, depth+1)
-
-        combi = []
+    for j in range(i+1, n+1):
+        combi.append(j)
+        f(n, m, depth+1, j, combi)
+        combi.pop()
 
 N, M = map(int, input().split())
 
-num = []
-combi = []
-f(N,M,0)
-print(num)
+f(N, M)
+
+
