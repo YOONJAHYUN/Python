57e0ef111184c8283e2b54b74bf0c6d7b648b24c
YOONJAHYUN <dbswkgus323@gmail.com>
2023-02-27 13:31:27 +0900

    0227

---
+++ BOJ/2578.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+bing_go = [list(map(int, input().split())) for _ in range(5)]
+
+for _ in range(5):
+    let_s_bing = map(int, input().split())
+
+    for number in let_s_bing:
+
