c29d1ddfd2d66d415f88dcd6923c2d8a901254cd
YOONJAHYUN <dbswkgus323@gmail.com>
2023-03-02 20:51:10 +0900

    0302

--- BOJ/2527.py
+++ BOJ/2527.py
@@ -10,33 +10,23 @@ for _ in range(4):
 
     x1, y1, p1, q1, x2, y2, p2, q2 = map(int, input().split())
 
-    # 직사각형
-    if (x1 <= x2 <= p1 and y1 <= y2 <= q1) or(x1 <= p2 <= p1 and y1 <= q2 <= q1) or (x2 <= x1 <= p2 and y2 <= y1 <= q2) or(x2 <= p1 <= p2 and y2 <= q1 <= q2) or (x1<=x2<=p1 and x1<=p2<=p1 and y1<=y2<=q2 and y1<=q2):
-        if (x1 == p2 and q1>y2>y1) or (y1 == q2 and x1<x2<p1) or (p1 == x2 and y1 < y2 < q1)or (p1 == y2 and x1<x2<p1) :
-            print('b')
-        elif (p1 == x2 and q1 == y2) or (x1 == p2 and q1 == y2) or (x1 == p2 and y1 == q2) or (q1 == x2 and y1 == q2):
+    square1_x = set(range(x1, p1+1))
+    square1_y = set(range(y1, q1+1))
+
+    square2_x = set(range(x2, p2+1))
+    square2_y = set(range(y2, q2+1))
+
+    x = square2_x & square1_x   # x 교집합
+    y = square1_y & square2_y   # y 교집합
+
+
+    if not all([x, y]):
+        print('d')
+    else:
+        if len(x) == 1 and len(y) == 1:
             print('c')
+        elif len(x) == 1 or len(y) == 1:
+            print('b')
         else:
             print('a')
-    # 선분이 되는 조건
-    elif (x1 == p2 and q1>y2>y1) or (y1 == q2 and x1<x2<p1) or (p1 == x2 and y1 < y2 < q1)or (p1 == y2 and x1<x2<p1) :
-        print('b')
-
-
-    # 점이 되는 조건
-    elif (p1 == x2 and q1 == y2) or (x1 == p2 and q1 == y2) or (x1 == p2 and y1 == q2) or (q1 == x2 and y1 == q2):
-        print('c')
-    # 공통부분이 없음
-    # elif p1 < x2 or q1 < y2 or y2 < q1 or p2 < x1:
-    #     print('d')
-
-    else:
-        print('d')
-
-'''
-2 2 3 3 3 3 4 4   
-2 2 3 3 1 1 2 2
-2 2 3 3 3 1 4 2
-2 2 3 3 1 3 2 4
-'''
 
