4e5ed04a4fad8df7390deb0b0e1ea9914c0fe17a
YOONJAHYUN <dbswkgus323@gmail.com>
2023-03-10 13:55:36 +0900

    0310

--- BOJ/5639.py
+++ BOJ/5639.py
@@ -1,10 +1,26 @@
 import sys
+input = sys.stdin.readline
 
-# def postorder_traverse(T):
-# 	if T:
-# 	    postorder_traverse(T.left)
-# 	    postorder_traverse(T.right)
-# 		visit(T)
+def make_tree(data):
+    global tree
+
+    if len(data) > 1:
+        tree.append(data[1])
+    else:
+        return
+
+    left = [0]
+    right = [0]
+
+    for i in range(2, len(data)):
+        if data[1] < data[i]:
+            right.append(data[i])
+        else:
+            left.append(data[i])
+    if left:
+        make_tree(left)
+    if right:
+        make_tree(right)
 
 
 # numbers는 전위순회한 값으로 되어있다.
@@ -17,25 +33,28 @@ while True:
     except:
         break
 
+tree = [0]
+
 print(numbers)
+make_tree(numbers)
+
 N = len(numbers)
 # 전위순회한 값을 다시 원래 트리로 만들어주자.
-tree = [0]*(N+1)
-tree[1] = numbers[1]
-idx = 1
-
-left = []
-right = []
-
-for i in range(2, N):
-    if tree[1] < numbers[i]:
-        right.append(numbers[i])
-    else:
-        left.append(numbers[i])
 
 
-print(right)
-print(left)
+print(tree)
+# left = []
+# right = []
+#
+# for i in range(2, N):
+#     if tree[1] < numbers[i]:
+#         right.append(numbers[i])
+#     else:
+#         left.append(numbers[i])
+#
+#
+# print(right)
+# print(left)
 
 '''
 [0, 50, 30, 24, 5, 28, 45, 98, 52, 60]
