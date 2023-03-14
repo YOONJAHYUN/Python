e7d55d7c4e716073ecca166beba4819e11d999da
YOONJAHYUN <dbswkgus323@gmail.com>
2023-03-14 11:21:24 +0900

    14888 done

--- BOJ/14888.py
+++ BOJ/14888.py
@@ -5,38 +5,44 @@ input = sys.stdin.readline
 N = int(input())
 numbers = list(map(int, input().split()))
 multi = list(map(int, input().split()))
-lst = []
-for i in range(4):
-    for j in range(multi[i]):
+lst = multi[0]*'+' + multi[1]*'-' + multi[2]*'*'+ multi[3]*'/'
 
-        if i == 0:
-            lst.append('+')
-        elif i == 1:
-            lst.append(multi[i]*'-')
-        elif i == 2:
-            lst.append(multi[i]*'*')
-        else:
-            lst.append(multi[i]*'/')
-
-if len(lst) == 1:
-    per = lst
-else:
-    per = list(permutations(lst, N-1))
-
-print(per)
+per = list(permutations(lst, N-1))
 
 my_max = -1000000000
 my_min = 1000000000
 
-result = []
-result.append(numbers[0])
-result.append(numbers[1])
+for i in range(len(per)):
+    ans = 0
+    num1 = numbers[0]
+    num2 = numbers[1]
+    for j in range(N-1):
+        if j == 0:
+            if per[i][j] == '+':
+                ans = num1 + num2
+            elif per[i][j] == '-':
+                ans = num1 - num2
+            elif per[i][j] == '*':
+                ans = num1 * num2
+            elif per[i][j] == '/':
+                ans = int(num1 / num2)
+        else:
+            num = numbers[j+1]
+            if per[i][j] == '+':
+                ans = ans + num
+            elif per[i][j] == '-':
+                ans = ans - num
+            elif per[i][j] == '*':
+                ans = ans * num
+            elif per[i][j] == '/':
+                ans = int(ans / num)
 
-for i in range(N-1):
 
+    if ans > my_max:
+        my_max = ans
 
-
-
+    if ans < my_min:
+        my_min = ans
 
 print(my_max)
 print(my_min)
