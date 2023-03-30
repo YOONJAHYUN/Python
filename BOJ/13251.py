65825809bb6cf53a7f917e3646252a370c0c45e7
YOONJAHYUN <dbswkgus323@gmail.com>
2023-03-30 17:53:12 +0900

    0330

--- BOJ/13251.py
+++ BOJ/13251.py
@@ -2,23 +2,36 @@ import sys
 input = sys.stdin.readline
 
 # 모두 같은 색일 확률
-def rock(m,k):
 
-    result = 0
-    color_sum = sum(N_data)
-    for color in N_data:
-        ans = 1
-        if color >= k:
-            for i in range(m):
-                ans = ans * ((color-i)/color_sum)
-
-    result += ans
-    return result
-# M 조약돌 종류 / N 조약돌 개수 / K 랜덤하게 뽑을 조약돌
+# M 조약돌 종류 / N 각각 조약돌 개수 / K 랜덤하게 뽑을 조약돌
 M = int(input())
 N_data = list(map(int, input().split()))
+
+# 전체 조약돌 개수
+N = sum(N_data)
+
 K = int(input())
 
+'''
+(색 남은 양) / (전체 개수)
 
-print(rock(M, K))
+색 남은 양에서 하나씩 빼기
+'''
+
+result = 0
+
+for i in range(M):
+    ans = 1
+    idx = 0
+    if N_data[i] >= K:
+        while idx < K:
+            ans = ans * ((N_data[i] - idx) / (N - idx))
+            idx += 1
+
+        result += ans
+
+
+print(result)
+
+
 
---
+++ BOJ/9663.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+def queen(num):
+
+
+
+    pass
+
+
+N = int(input())
+queen(N)
